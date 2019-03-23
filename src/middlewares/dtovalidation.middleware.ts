a225e04981a16372a41608c36f013006ab7ccbf2
MaxySpark <me@maxyspark.me>
2019-03-24 05:10:10 +0530

    typescript compile error

--- src/middlewares/dtovalidation.middleware.ts
+++ src/middlewares/dtovalidation.middleware.ts
@@ -8,7 +8,8 @@ function validationMiddleware<T>(type: any, skipMissingProperties = false): Requ
     validate(plainToClass(type, req.body), { skipMissingProperties })
       .then((errors: ValidationError[]) => {
         if (errors.length > 0) {
-          const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
+          const message = errors.map((error: ValidationError) => Object.keys(error.constraints).map(key => error.constraints[key])).join(', ');
+          // Object.values(error.constraints)
           next(new HttpException(400, message));
         } else {
           next();
