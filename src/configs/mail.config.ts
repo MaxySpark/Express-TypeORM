663a00d6ceebcd5f5720b5885d2e7f95588e6c80
Bhargab Hazarika <me@maxyspark.me>
2019-04-06 01:11:20 +0530

    nodemailer Mail

--- src/configs/mail.config.ts
+++ src/configs/mail.config.ts
@@ -1,4 +1,5 @@
 const MailConfig = {
+    googleMailUserEmail     : process.env.GOOGLE_MAIL_USER_EMAIL,
     googleMailClientId      : process.env.GOOGLE_MAIL_CLIENT_ID,
     googleMailClientSecret  : process.env.GOOGLE_MAIL_CLIENT_SECRET,
     googleMailRefreshToken  : process.env.GOOGLE_MAIL_REFRESH_TOKEN
--- src/utils/Mail.util.ts
+++ src/utils/Mail.util.ts
@@ -2,12 +2,19 @@ import * as nodemailer from 'nodemailer';
 import MailConfig from '../configs/mail.config';
 import { google } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
+import { Options } from 'nodemailer/lib/mailer';
+import { getRepository } from 'typeorm';
+import { Mail as MailEntity } from '../db/entities/Mail.entity';
 
 class Mail {
+    private mailRepository = getRepository(MailEntity);
     private oAuth2Client: OAuth2Client;
+    private smtpTransport: nodemailer.Transporter;
+    public mailOptions: Options;
 
     constructor() {
         this.initializeGoogleOauth2Client();
+        this.initializeSMTPConfiguration();
     }
 
     private initializeGoogleOauth2Client() {
@@ -21,4 +28,43 @@ class Mail {
             refresh_token: MailConfig.googleMailRefreshToken
         });
     }
+
+    private async initializeSMTPConfiguration() {
+        try {
+            const refreshTokenResponse = await this.oAuth2Client.refreshAccessToken();
+            const accessToken = refreshTokenResponse.credentials.access_token
+
+            this.smtpTransport = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: MailConfig.googleMailUserEmail,
+                    clientId: MailConfig.googleMailClientId,
+                    clientSecret: MailConfig.googleMailClientSecret,
+                    refreshToken: MailConfig.googleMailRefreshToken,
+                    accessToken: accessToken
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+
+    }
+
+    public async send() {
+        try {
+            const mail = await this.smtpTransport.sendMail(this.mailOptions);
+            const new_mail = this.mailRepository.create({
+                messageId: mail.messageId,
+                recipient: mail.envelope.to[0]
+            });
+            
+            this.mailRepository.save(new_mail);
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
 }
