c507000c48df73cefe92b17fbe4f88601c1bc324
MaxySpark <me@maxyspark.me>
2019-05-17 03:00:27 +0530

    script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import App from './App'
-import  { getRoutes }  from './app.routes';
+import  { getRoutes }  from './App.routes';
 import { createConnection } from 'typeorm';
 import dbConncetionOptions from './configs/orm.config';
 import AppConfig from './configs/app.config';
