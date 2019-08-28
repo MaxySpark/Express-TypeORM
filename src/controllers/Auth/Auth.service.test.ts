68cc128ff4067a3b085e1ba3d0c37e1abcdfbdbb
MaxySpark <me@maxyspark.me>
2019-08-28 23:13:09 +0530

    auth test reset password

--- src/controllers/Auth/Auth.service.test.ts
+++ src/controllers/Auth/Auth.service.test.ts
@@ -1,17 +1,19 @@
 import AuthService from './Auth.service';
-import { IJwtPayload } from '../../interfaces/JwtPayload.interface';
-import * as jwt from 'jsonwebtoken';
-import { RegisterDto } from './Auth.dto';
-import { getConnection, getConnectionManager, createConnection } from "typeorm";
+import { RegisterDto, LoginDto, ResetPasswordDto } from './Auth.dto';
+import { getConnection, getConnectionManager, getRepository, Repository } from "typeorm";
 import { dbConncetionOptions } from '../../configs/orm.config';
+import { ResetPassword } from '../../db/entities/ResetPassword.entity';
 
 describe('Auth Service', () => {
     let authService: AuthService;
+    let resetPasswordRepository: Repository<ResetPassword>;
     beforeAll(async () => {
         const connectionManager = getConnectionManager();
         const connection = connectionManager.create(dbConncetionOptions);
         await connection.connect();
+
         authService = new AuthService();
+        resetPasswordRepository = getRepository(ResetPassword);
     });
 
     afterAll(async () => {
@@ -19,19 +21,83 @@ describe('Auth Service', () => {
     });
 
 
-    describe('Create User', () => {
-        it('should return a string', async () => {
-            const user: RegisterDto = {
-                email: 'test@test.com',
-                firstname: 'Test',
-                lastname: 'Test',
-                password: '1234567890',
-                username: 'testuser'
+    describe('Register User', () => {
+        const user: RegisterDto = {
+            email: 'test@test.com',
+            firstname: 'Test',
+            lastname: 'Test',
+            password: 'password',
+            username: 'testuser'
 
-            };
+        };
+
+        it('should return a string', async () => {
+
             const token = await authService.register(user);
 
             expect(token).toBeDefined();
+        });
+
+        it('should thow an error if email already exist', async () => {
+            try {
+                await authService.register(user);
+            } catch (e) {
+                expect(e.status).toEqual(409)
+                expect(e.message).toEqual(`User with email ${user.email} already exists`)
+            }
+        });
+    });
+
+    describe('Login User', () => {
+        it('should return a string', async () => {
+            const user: LoginDto = {
+                email: 'test@test.com',
+                password: 'password',
+
+            };
+            const token = await authService.login(user);
+
+            expect(token).toBeDefined();
+        });
+
+        it('should thow an error if password is wrong', async () => {
+            try {
+                const user: LoginDto = {
+                    email: 'test@test.com',
+                    password: 'wrong_password',
+                };
+                await authService.login(user);
+            } catch (e) {
+                expect(e.status).toEqual(401)
+                expect(e.message).toEqual(`Email or Password Does not Matched`)
+            }
+        });
+    });
+
+    describe('Rreset Password', () => {
+
+        let resetPassword: ResetPassword;
+        const user: ResetPasswordDto = {
+            email: 'test@test.com'
+        };
+
+        const user2: ResetPasswordDto = {
+            email: 'test2@test.com'
+        };
+
+        it('should send generate a reset password object', async () => {
+            await authService.resetPassword(user);
+            resetPassword = await resetPasswordRepository.findOne({email: user.email});
+            expect(resetPassword.email).toEqual('test@test.com');
+        });
+
+        it('should throw an error if user not exist with email', async () => {
+            try {
+                await authService.resetPassword(user2);
+            } catch(e) {
+                expect(e.status).toEqual(404);
+                expect(e.message).toEqual(`User with email ${user2.email} does not exist`)
+            }
         });
     });
 });
